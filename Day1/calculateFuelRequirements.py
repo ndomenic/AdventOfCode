5f4b9b865755012339307b8c5a4072dc9ce2af43
Nick <ndomenic@uoguelph.ca>
2019-12-01 19:52:34 -0500

    Done part 2

--- Day1/calculateFuelRequirements.py
+++ Day1/calculateFuelRequirements.py
@@ -1,10 +1,16 @@
 import sys
 import math
 
+def calculateRecursively(num):
+    tmpNum = int(math.floor(int(num)/3)-2)
+    if (tmpNum >= 0):
+        return tmpNum + calculateRecursively(tmpNum)
+    return 0
+
 f = open(sys.argv[1], "r")
 sum = 0
 
 for line in f:
-  sum += math.floor(int(line)/3)-2
+    sum += calculateRecursively(line)
 
 print("Sum: " + str(sum))
